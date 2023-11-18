b1a818d3ec2d2be2349a94d8013a249b04fd7013
orayanics <nicole.oraya.cics@ust.edu.ph>
2023-11-18 09:40:56 +0800

    identify user access for nav
    To Implement in activity_post, admin_feed (DELETE), layout_profile

--- app/src/main/java/com/example/muzikup/FeedActivity.kt
+++ app/src/main/java/com/example/muzikup/FeedActivity.kt
@@ -2,8 +2,11 @@ package com.example.muzikup
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 
 class FeedActivity : AppCompatActivity() {
@@ -13,5 +16,25 @@ class FeedActivity : AppCompatActivity() {
 
         val btnHome : ImageView = findViewById(R.id.footerHome)
         btnHome.setColorFilter(Color.parseColor("#40AA4A"))
+
+        identifyAccess("USER")
+    }
+
+    // Implement to activity_post, admin_feed (DELETE), layout_profile
+    fun identifyAccess(access : String){
+        val cardView : CardView = findViewById(R.id.footerId)
+
+        val userAccess = access
+
+        val layoutResId: Int = if (userAccess == "USER") {
+            R.layout.footer_user
+        } else {
+            R.layout.footer_admin
+        }
+        // Inflate and replace the included layout
+        val inflater = LayoutInflater.from(this)
+        val newIncludedLayout = inflater.inflate(layoutResId, cardView, false)
+        cardView.removeAllViews() // Remove the previous included layout
+        cardView.addView(newIncludedLayout)
     }
 }
