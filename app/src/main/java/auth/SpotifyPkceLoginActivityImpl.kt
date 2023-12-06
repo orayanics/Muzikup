e1ee41280894dac96317ff476bb0b74b960b7d0e
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-12-07 05:31:31 +0800

    Review to firebase, Login (userID) -Only accepts 1 account error

--- app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
+++ app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
@@ -1,19 +1,14 @@
 package auth
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
 import com.adamratzman.spotify.SpotifyClientApi
-import com.adamratzman.spotify.SpotifyException
 import com.adamratzman.spotify.SpotifyScope
 import com.adamratzman.spotify.auth.pkce.AbstractSpotifyPkceLoginActivity
 import com.example.muzikup.BuildConfig
 import com.example.muzikup.SearchFeedActivity
 import com.example.muzikup.SpotifyPlaygroundApplication
-import data.Model
-import utils.safeLet
 import utils.toast
 
 internal var pkceClassBackTo: Class<out Activity>? = null
@@ -32,18 +27,15 @@ class SpotifyPkceLoginActivityImpl : AbstractSpotifyPkceLoginActivity() {
         startActivity(Intent(this, classBackTo))
     }
 
+    suspend fun getId(api: SpotifyClientApi) {
+        val user = api.getUserId()
+        toast("Authentication has completed! $user")
+    }
+
     override fun onFailure(exception: Exception) {
         exception.printStackTrace()
         pkceClassBackTo = null
         Log.d("loginerror", "${exception.message}.")
         toast("Auth failed: ${exception.message}")
-    }
-}
-
-fun toast(context: Context?, message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    safeLet(context, message, duration) { safeContext, safeMessage, safeDuration ->
-        (safeContext as? Activity)?.runOnUiThread {
-            Toast.makeText(safeContext, safeMessage, safeDuration).show()
-        }
     }
 }
--- app/src/main/java/com/example/muzikup/LoginActivity.kt
+++ app/src/main/java/com/example/muzikup/LoginActivity.kt
@@ -1,13 +1,14 @@
 package com.example.muzikup
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import auth.SpotifyPkceLoginActivityImpl
 import com.adamratzman.spotify.auth.pkce.startSpotifyClientPkceLoginActivity
 
+
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -16,9 +17,7 @@ class LoginActivity : AppCompatActivity() {
 
         btnLogin.setOnClickListener {
             onConnectSpotifyPKCEClick()
-//            val i = Intent(this, RegisterActivity::class.java)
-//            startActivity(i)
-//            finish()
+
         }
     }
 
