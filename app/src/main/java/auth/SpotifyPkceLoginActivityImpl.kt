4e2beade3d2384f329753d0c70aa201f6d9bb2d2
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-12-06 21:44:58 +0800

    revert

--- app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
+++ app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
@@ -11,33 +11,12 @@ import com.adamratzman.spotify.auth.pkce.AbstractSpotifyPkceLoginActivity
 import com.example.muzikup.BuildConfig
 import com.example.muzikup.SearchFeedActivity
 import com.example.muzikup.SpotifyPlaygroundApplication
-import api.UserProfile
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
 import utils.safeLet
 import utils.toast
 
 internal var pkceClassBackTo: Class<out Activity>? = null
 
-interface SpotifyApiService {
-    @GET("/v1/users/{userId}")
-    fun getUserProfile(@Path("userId") userId: String): Call<UserProfile>
-}
-
 class SpotifyPkceLoginActivityImpl : AbstractSpotifyPkceLoginActivity() {
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://api.spotify.com")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val spotifyApiService: SpotifyApiService = retrofit.create(SpotifyApiService::class.java)
-
     override val clientId = BuildConfig.SPOTIFY_CLIENT_ID
     override val redirectUri = BuildConfig.SPOTIFY_REDIRECT_URI_PKCE
     override val scopes = SpotifyScope.values().toList()
@@ -48,43 +27,12 @@ class SpotifyPkceLoginActivityImpl : AbstractSpotifyPkceLoginActivity() {
         val classBackTo = pkceClassBackTo ?: SearchFeedActivity::class.java
         pkceClassBackTo = null
         toast("Authentication has completed. Launching ${classBackTo.simpleName}..")
-
-        // Fetch user information
-        GlobalScope.launch {
-            // Fetch user information
-            fetchUserInfo(api)
-
-            startActivity(Intent(this@SpotifyPkceLoginActivityImpl, classBackTo))
-        }
+        startActivity(Intent(this, classBackTo))
     }
+
     suspend fun getId(api: SpotifyClientApi) {
         val user = api.getUserId()
         toast("Authentication has completed! $user")
-    }
-
-    suspend fun fetchUserInfo(api: SpotifyClientApi) {
-        val userId = api.getUserId()
-
-        // Make the API call using Retrofit
-        val call = spotifyApiService.getUserProfile(userId)
-        try {
-            val response = call.execute()
-
-            Log.d("UserInfo", "Request URL: ${call.request().url}")
-            Log.d("UserInfo", "Request Headers: ${call.request().headers}")
-
-            if (response.isSuccessful) {
-                val user = response.body()
-                val userEmail = user?.email
-                val userPhotoUrl = user?.images?.firstOrNull()?.url
-
-                Log.d("UserInfo", "Email: $userEmail, Photo: $userPhotoUrl")
-            } else {
-                Log.d("UserInfo", "Failed to get user profile: ${response.code()}")
-            }
-        } catch (e: Exception) {
-            Log.d("UserInfo", "Exception while getting user profile: ${e.message}")
-        }
     }
 
     override fun onFailure(exception: Exception) {
