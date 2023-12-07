f8b1f30f5a5a8f63fd241281d59a17fd99afdf7f
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-12-07 09:00:51 +0800

    getclientprofile userid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,4 +114,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     implementation("androidx.navigation:navigation-compose:2.4.1")
     implementation("androidx.window:window:1.0.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
 }
--- app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
+++ app/src/main/java/auth/SpotifyPkceLoginActivityImpl.kt
@@ -9,6 +9,9 @@ import com.adamratzman.spotify.auth.pkce.AbstractSpotifyPkceLoginActivity
 import com.example.muzikup.BuildConfig
 import com.example.muzikup.SearchFeedActivity
 import com.example.muzikup.SpotifyPlaygroundApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import utils.toast
 
 internal var pkceClassBackTo: Class<out Activity>? = null
@@ -16,32 +19,45 @@ internal var pkceClassBackTo: Class<out Activity>? = null
 class SpotifyPkceLoginActivityImpl : AbstractSpotifyPkceLoginActivity() {
     override val clientId = BuildConfig.SPOTIFY_CLIENT_ID
     override val redirectUri = BuildConfig.SPOTIFY_REDIRECT_URI_PKCE
-    override val scopes = SpotifyScope.values().toList()
+    //override val scopes = SpotifyScope.values().toList()
+    override val scopes = (SpotifyScope.values() + SpotifyScope.USER_READ_EMAIL + SpotifyScope.USER_READ_PRIVATE).toList()
+
 
     override fun onSuccess(api: SpotifyClientApi) {
         val model = (application as SpotifyPlaygroundApplication).model
         model.credentialStore.setSpotifyApi(api)
         val classBackTo = pkceClassBackTo ?: SearchFeedActivity::class.java
-        resetStateForNewUser()
-       // pkceClassBackTo = null
+        pkceClassBackTo = null
         toast("Authentication has completed. Launching ${classBackTo.simpleName}..")
 
         startActivity(Intent(this, classBackTo))
+
+        // Create a CoroutineScope
+        val coroutineScope = CoroutineScope(Dispatchers.Main)
+        coroutineScope.launch {
+            getId(api)
+        }
+
+    }
+    private suspend fun getId(api: SpotifyClientApi) {
+        try {
+            val userId = api.users.getClientProfile().id
+           // val userId = userPrivate.id
+            //val email = userPrivate.email
+            //val profilePictureUrl = userPrivate.images?.firstOrNull()?.url
+
+            Log.d("loginerror", "User's ID: $userId")
+          //  Log.d("loginerror", "User's Email: $email")
+           // Log.d("loginerror", "Profile Picture URL: $profilePictureUrl")
+
+            toast("User's ID: $userId\nUser's Email: ")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("loginerror", "API Error: ${e.message}.")
+            toast("Error fetching user information: ${e.message}")
+        }
     }
 
-    private fun resetStateForNewUser() {
-        // Reset any state related to the previous user
-        pkceClassBackTo = null
-
-        // Clear existing tokens and set the credential store to a clean state
-//        val model = (application as SpotifyPlaygroundApplication).model
-//        model.credentialStore.clear()
-    }
-
-     suspend fun getId(api: SpotifyClientApi) {
-        val user = api.getUserId()
-        toast("Authentication has completed! $user")
-    }
 
     override fun onFailure(exception: Exception) {
         exception.printStackTrace()
@@ -50,3 +66,4 @@ class SpotifyPkceLoginActivityImpl : AbstractSpotifyPkceLoginActivity() {
         toast("Auth failed: ${exception.message}")
     }
 }
+
